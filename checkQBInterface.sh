05d2a8ddca1810d537740adf347be1ca279d001d
S0me6uy <64698798+S0me6uy@users.noreply.github.com>
2021-08-15 22:34:56 +0200

    move network command into sudo mode. Avoid qbt restart by pause and resume all torrents.

--- checkQBInterface.sh
+++ checkQBInterface.sh
@@ -2,12 +2,14 @@
 
 localhostname = test.com
 
-if [ "$(curl -s -w %{http_code} ifconfig.me --connect-timeout 25 -m 25)" == "000" ] || [ "$(host $localhostname | cut -d " " -f 4)" == "$(curl ifconfig.me)" ]
+if [ "$(curl -s -w %{http_code} ifconfig.me --connect-timeout 55 -m 55)" == "000" ] || [ "$(host $localhostname | cut -d " " -f 4)" == "$(curl ifconfig.me)" ]
 then
         # fall state
-        wg-quick down server
-        wg-quick up server
-        service qbittorrent restart
+        sudo wg-quick down server
+        sudo wg-quick up server
+#        sudo service qbittorrent restart
+        qbt torrent pause all
+        qbt torrent resume all
         curl --location --request POST 'https://api.telegram.org/bot<Bot ID>/sendMessage' \
                 --header 'Content-Type: application/json' \
                 --data-raw '{"chat_id" : "-<Chat ID>","text" : "VPN Interface has restart.\nEverything is fine if you are able to see this message!"}'\
